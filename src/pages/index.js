bef2b1b54fb15a2e2ad744c41b5c0094796f43c7
Nishanth Samala <nishanth.samala@gmail.com>
2020-04-13 16:36:45 -0400

    more responsive profile pic fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ export default ({ data, location }) => {
         <div className="columns">
           <div className="column col-12">
             <div className="columns">
-              <div className="column col-1 col-sm-6 col-mx-auto">
+              <div className="column col-1 col-sm-7 col-md-3 col-mx-auto">
                 <Img fluid={me} fadeIn={true} alt="Nishanth" className="s-circle" />
               </div>
               <div className="column col-mx-auto">
