0f0170e0ec538d931186c5a753fccd2b678cf024
Nishanth Samala <nishanth.samala@gmail.com>
2020-04-14 14:54:34 -0400

    ui consistency

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -7,10 +7,11 @@ import styles from './article-preview.module.css'
 export default ({ article }) => (
   <div className={styles.preview}>
     <Link to={`/blog/${article.slug}`}><Img alt={article.title} fluid={article.heroImage.fluid} /></Link>
-    <h4 className={styles.previewTitle}>
+    <h4 className={`${styles.previewTitle} post-header`}>
       <Link to={`/blog/${article.slug}`}>{article.title}</Link>
     </h4>
-    <small className="text-gray">{article.publishDate} • { timeToRead(article) }</small>
+    <small className="text-gray">{article.publishDate} • {timeToRead(article)}</small>
+    <div className='divider'></div>
     <div
       dangerouslySetInnerHTML={{
         __html: article.description.childMarkdownRemark.html,
