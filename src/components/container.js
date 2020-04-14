5e5173dd5161fe1c5cf6f21d22d29a8781260041
Nishanth Samala <nishanth.samala@gmail.com>
2020-04-14 12:39:36 -0400

    testing out a new breakpoint for large screens

--- src/components/container.js
+++ src/components/container.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default ({ children }) => (
   <div className="container">
     <div className="columns">
-      <div className="column col-9 col-sm-11 col-md-11 col-lg-9 col-xl-9 col-mx-auto">
+      <div className="column col-9 col-sm-11 col-md-11 col-lg-9 col-xl-6 col-mx-auto">
         {children}
       </div>
     </div>
