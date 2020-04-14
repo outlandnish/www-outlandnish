f97363c705a00f0d34f5423b2195759f5cf23c0f
Nishanth Samala <nishanth.samala@gmail.com>
2020-04-14 13:24:20 -0400

    updated logo to use outlines instead of font reference

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Logo from '../images/logo.svg'
+import Logo from '../images/outlandnish.svg'
 import styles from './navigation.module.css'
 
 export default () => (
