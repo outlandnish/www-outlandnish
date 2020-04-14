dbafc1b1d8e695b860af4d859b2cb520d24ee5e1
Nishanth Samala <nishanth.samala@gmail.com>
2020-04-14 15:09:22 -0400

    added linkedin. made footer component

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { FaInstagram, FaGithub, FaSpotify, FaLinkedin } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
+
+export default () => (
+  <section>
+    <div className="divider"></div>
+    <div className="footer text-gray text-center">
+      <a href="https://github.com/outlandnish"  target="_blank" className="btn btn-link social-link"><FaGithub /></a>
+      <a href="https://linkedin.com/in/nishanthsamala"  target="_blank" className="btn btn-link social-link"><FaLinkedin /></a>
+      <a href="https://instagram.com/outlandnish" target="_blank" className="btn btn-link social-link"><FaInstagram /></a>
+      <a href="https://open.spotify.com/user/nishanthsamala?si=hdglQJ9LQlKRSQkxANMrhg"  target="_blank" className="btn btn-link social-link"><FaSpotify /></a>
+      <a href="mailto://hey@outlandnish.com"  target="_blank" className="btn btn-link social-link"><MdEmail /></a>
+      <section className="text-primary">Outlandnish &copy; 2020</section>
+    </div>
+  </section>
+)
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,12 @@
 import React from 'react'
-import { Link } from 'gatsby'
 import base from './base.scss'
 import Container from './container'
 import Navigation from './navigation'
-
-import { FaInstagram, FaGithub, FaSpotify } from 'react-icons/fa'
-import { MdEmail } from 'react-icons/md'
+import Footer from './footer'
 
 class Template extends React.Component {
   render() {
     const { location, children } = this.props
-    let header
 
     let rootPath = `/`
     if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
@@ -21,14 +17,7 @@ class Template extends React.Component {
       <Container>
         <Navigation />
         {children}
-        <div className="divider"></div>
-        <div className="footer text-gray text-center">
-          <a href="https://instagram.com/outlandnish" target="_blank" className="btn btn-link social-link"><FaInstagram /></a>
-          <a href="https://github.com/outlandnish"  target="_blank" className="btn btn-link social-link"><FaGithub /></a>
-          <a href="https://open.spotify.com/user/nishanthsamala?si=hdglQJ9LQlKRSQkxANMrhg"  target="_blank" className="btn btn-link social-link"><FaSpotify /></a>
-          <a href="mailto://hey@outlandnish.com"  target="_blank" className="btn btn-link social-link"><MdEmail /></a>
-          <section className="text-primary">Outlandnish &copy; 2020</section>
-        </div>
+        <Footer />
       </Container>
     )
   }
