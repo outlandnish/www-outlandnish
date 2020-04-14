e7348315b6cd9c75505487479314f2fc089697bd
Nishanth Samala <nishanth.samala@gmail.com>
2020-04-14 15:10:18 -0400

    summary edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,14 +30,14 @@ export default ({ data, location }) => {
           </div>
         </div>
       <section className={styles.summary}>
-        <h5>Here's some other things you'll find me doing:</h5>
+        <h5>Here's some other things you'll find me:</h5>
         <ul className="summary-list">
           <li>Bringing <Link to="/hacks">fun project ideas</Link> to life and contributing to <a href="https://github.com/outlandnish">open source</a></li>
           <li><a href="https://racing.outlandnish.com" target="_blank">Time attack competition and drift exhibitions</a></li>
           <li>Co-hosting the <a href="http://brakefastclub.com" target="_blank">Brakefast Club</a> podcast</li>
-          <li>Any form of <a href="https://instagram.com/outlandnish" target="_blank">outdoor adventure</a></li>
+          <li>Doing any form of <a href="https://instagram.com/outlandnish" target="_blank">outdoor adventure</a></li>
           <li>E-skating, <a href="https://www.meetup.com/windycityboarders/" target="_blank">snowboarding</a>, and trail riding</li>
-          <li>Travel, though I've been pretty bad at documenting it</li>
+          <li>Traveling, though I've been pretty bad at documenting it</li>
           <li>Wandering to intimate concert venues and <a href="https://open.spotify.com/user/nishanthsamala?si=hdglQJ9LQlKRSQkxANMrhg" target="_blank">discovering music</a></li>
         </ul>
         <p>Wanna chat or collaborate? Reach out and <a href="mailto://hey@outlandnish.com">say hey.</a></p>
