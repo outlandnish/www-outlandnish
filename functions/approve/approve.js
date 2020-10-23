dc75f3fa01696e93b931bb99282048ee0c3f3d4b
Nishanth Samala <nishanth.samala@gmail.com>
2020-10-23 14:04:12 -0400

    update approve function

--- functions/approve/approve.js
+++ functions/approve/approve.js
@@ -1,11 +1,19 @@
 const password = process.env.APPROVE_PASSWORD
 
-exports.handler = (event, context) => {
+exports.handler = async (event, context) => {
+  if (!json.body)
+    return { statusCode: 400, body: JSON.stringify({ error: 'requires password in body' }) }
+  
+  try {
   let { password: userPassword } = JSON.parse(event.body)
   console.log(`approve ${userPassword} against ${password}: ${userPassword === password}`)
 
   if (userPassword === password)
     return { statusCode: 200, body: JSON.stringify({ approved: true })}
   else
-    return { statusCode: 401, body: JSON.stringify({ approved: false })}
+    return { statusCode: 401, body: JSON.stringify({ approved: false  })}
+  }
+  catch (err) {
+    return { statusCode: 400, body: JSON.stringify({ error: err })}
+  }
 }
